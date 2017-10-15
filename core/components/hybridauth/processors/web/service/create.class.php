7301a746d92e671d598ae21550e11c479d6c54c2
Jim Graham <info@sparkmediagroup.com>
2017-10-15 09:36:34 -0400

    Update create.class.php
    Account for instances where a property value is an array. Before this change, a php warning would be thrown when modx->stripTags was called on an array value.

--- core/components/hybridauth/processors/web/service/create.class.php
+++ core/components/hybridauth/processors/web/service/create.class.php
@@ -16,7 +16,15 @@ class haUserServiceCreateProcessor extends modObjectCreateProcessor
 
         foreach ($properties as $k => $v) {
             $k = strtolower($k);
-            $properties[$k] = $this->modx->stripTags($v);
+            if(is_array($v)){
+                $tmp = array();
+                foreach ($v as $key => $val) {
+                    $tmp[$key] = $this->modx->stripTags($val);
+                }
+                $properties[$k] = $tmp;
+            } else {
+                $properties[$k] = $this->modx->stripTags($v);
+            }
         }
         $properties['createdon'] = date('Y-m-d H:i:m');
 
